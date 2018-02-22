dc7090090d24ea9c25a3ca6c70a3e8b95f583889
Andrew J <ImperialXT@users.noreply.github.com>
2018-02-22 20:06:13 +1100

    Change URL
    Let's use https://random.dog/woof.json instead as the pictures are not just stock images.

--- getDogPic.py
+++ getDogPic.py
@@ -5,7 +5,7 @@ from botocore.vendored import requests
 sns_topic = boto3.resource('sns').Topic(os.environ['SNS_TOPIC'])
 
 def lambda_handler(event, context):
-    response = requests.get("https://dog.ceo/api/breeds/image/random").json()
-    pic_url = response['message']
+    response = requests.get("https://random.dog/woof.json").json()
+    pic_url = response['url']
     
     sns_topic.publish(Message=pic_url)
